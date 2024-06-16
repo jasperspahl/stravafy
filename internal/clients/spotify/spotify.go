f7cc3d3887895ac2720dea07a66da74037dae062
Jasper Spahl <jasperspahl@web.de>
2024-06-16 13:19:54 +0200

    moved spotify client

---
+++ internal/clients/spotify/spotify.go
@@ -0,0 +1,83 @@
+package spotify
+
+import (
+	"errors"
+	"encoding/json"
+	"context"
+	"golang.org/x/oauth2"
+	"net/http"
+	"stravafy/internal/config"
+)
+
+
+type Client struct {
+	httpClient *http.Client
+}
+
+func NewSpotifyClient(token oauth2.Token) *Client {
+	oauth2Config := config.GetSpotifyOauthConfig()
+	return &Client{
+		httpClient: oauth2Config.Client(context.Background(), &token),
+	} 
+}
+
+func (c *Client) GetPlaylist(href string) (*MinimalPlaylist, error) {
+	resp, err := c.httpClient.Get(href + "?fields=name,owner.display_name")
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("playlist not found")
+	}
+	decoder := json.NewDecoder(resp.Body)
+	var pl MinimalPlaylist
+	err = decoder.Decode(&pl)
+	if err != nil {
+		return nil, err
+	}
+	return &pl, nil
+}
+
+func (c *Client) GetPlayerState() (int, *PlayerState, *ItemObject, *TrackObject, *EpisodeObject, error) {
+	resp, err := c.httpClient.Get("https://api.spotify.com/v1/me/player?additional_types=track,episode")
+	if err != nil {
+		return -1, nil, nil, nil, nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return resp.StatusCode, nil, nil, nil, nil, err
+	}
+	decoder := json.NewDecoder(resp.Body)
+
+	var ps PlayerState
+
+	err = decoder.Decode(&ps)
+	if err != nil {
+		return resp.StatusCode, nil, nil, nil, nil, err
+	}
+	if !ps.IsPlaying {
+		return resp.StatusCode, &ps, nil, nil, nil, nil
+	}
+	var item ItemObject
+	err = json.Unmarshal(ps.Item, &item)
+	if err != nil {
+		return resp.StatusCode, &ps, nil, nil, nil, err
+	}
+	switch item.Type {
+	case "track":
+		var track TrackObject
+		err = json.Unmarshal(ps.Item, &track)
+		if err != nil {
+			return resp.StatusCode, &ps, nil, nil, nil, err
+		}
+		return resp.StatusCode, &ps, &item, &track, nil, nil
+	case "episode":
+		var episode EpisodeObject
+		err = json.Unmarshal(ps.Item, &episode)
+		if err != nil {
+			return resp.StatusCode, &ps, nil, nil, nil, err
+		}
+		return resp.StatusCode, &ps, &item, nil, &episode, nil
+	default:
+		return resp.StatusCode, &ps, nil, nil, nil, errors.New("unknown item type")
+	}
+}
